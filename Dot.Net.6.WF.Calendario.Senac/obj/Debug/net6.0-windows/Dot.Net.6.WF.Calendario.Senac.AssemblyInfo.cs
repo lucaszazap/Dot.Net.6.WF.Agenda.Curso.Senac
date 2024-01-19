a856772c4b70e922021fae7278228723820e861b
caua.moreira <caua.moreira@TI-30>
2024-01-19 14:42:13 -0300

    instalador

--- Dot.Net.6.WF.Calendario.Senac/obj/Debug/net6.0-windows/Dot.Net.6.WF.Calendario.Senac.AssemblyInfo.cs
+++ Dot.Net.6.WF.Calendario.Senac/obj/Debug/net6.0-windows/Dot.Net.6.WF.Calendario.Senac.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dot.Net.6.WF.Calendario.Senac")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b0d6857432d8b619999eeb35eb6e1c35bd8c6a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d0265475354ba4fbdf07685f8477b10454b7ce9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dot.Net.6.WF.Calendario.Senac")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dot.Net.6.WF.Calendario.Senac")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
